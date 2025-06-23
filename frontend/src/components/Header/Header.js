cc640772cb02da4f6b99eb00f2e816e59a3faa55
hiraeth098 <breninhomc098@gmail.com>
2025-06-23 17:54:12 -0300

    Fix: Organização do escudo no meio da header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,25 +1,48 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './Header.module.css';
+import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 export default function Header() {
-    return (
-        <header className={styles.header}>
-            <div className={styles.container}>
-                <Link href="/"> 
-                {/* Escudo do CHBI: Link para a página inicial */}
-                <Image src="/CHBI.png" alt="Logo CHBI" width={235} height={170}/>
-                </Link >
-                <nav className={styles.nav}>
-                    <ul>
-                        <li><Link href="/">Início</Link></li>
-                        <li><Link href="/noticias">Notícias</Link></li>
-                        <li><Link href="/jogos">Jogos</Link></li>                       
-                        <li><Link href="/projetos-e-programas">Projetos e Programas</Link></li>
-                        <li><Link href="/contato">Contato</Link></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    
+    <header className={styles.headerWrapper}>
+      
+      <div className={styles.topBar}>
+        <div className={styles.container}>
+          <div className={styles.socialIcons}>
+            <a href="#" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+            <a href="#" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+            <a href="#" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
+          </div>
+        </div>
+      </div>
+
+      
+      <div className={styles.mainNav}>
+        <div className={styles.container}>
+          {/* 1. Links da Esquerda */}
+          <nav className={styles.navLinks}>
+            <Link href="/">Início</Link>
+            <Link href="/noticias">Notícias</Link>
+            <Link href="/jogos">Jogos</Link>
+          </nav>
+
+          
+          <div className={styles.logoContainer}>
+            <Link href="/">
+              <Image src="/CHBI.png" alt="Logo CHBI" width={280} height={186} />
+            </Link>
+          </div>
+
+          
+          <nav className={styles.navLinks}>
+            <Link href="/projetos-e-programas">Projetos</Link>
+            <Link href="/contato">Contato</Link>
+            
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 }
